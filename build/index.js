8290f9f677fb28862d1f9ab09447bf67bf576c71
Leonardo da Silva Miranda <leomiranda1995@gmail.com>
2024-07-09 18:43:53 -0300

    Configurando o Babel - React
    Configuração do React
    
    Instalação do preset do react do Babel para entender a transpilação do jsx
    > yarn add @babel/preset-react -D
    
    Instalação do React
    > yarn add react@^17.0.2 react-dom@^17.0.2

--- build/index.js
+++ build/index.js
@@ -1,5 +1,8 @@
 "use strict";
 
-var _user$address;
-var user = [];
-console.log(user === null || user === void 0 || (_user$address = user.address) === null || _user$address === void 0 ? void 0 : _user$address.street);
+var _react = _interopRequireDefault(require("react"));
+var _reactDom = _interopRequireDefault(require("react-dom"));
+function _interopRequireDefault(e) { return e && e.__esModule ? e : { "default": e }; }
+_reactDom["default"].render( /*#__PURE__*/_react["default"].createElement("h1", {
+  id: "title"
+}, /*#__PURE__*/_react["default"].createElement("span", null, "Ol\xE1 Mundo!")), document.getElementById('root'));
